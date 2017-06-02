07c94af9b4db74dce87c69660262de298dfe6c37
Matthew Hummer <biggreyson@gmail.com>
2017-06-02 10:20:48 -0600

    encode and decode pass

--- watneyDecoder.js
+++ watneyDecoder.js
@@ -1,12 +1,17 @@
 function decode(str){
-
+  return str.split(',').map((x)=>{
+    return String.fromCharCode(parseInt(x, 16));
+  }).join('')
 }
 
 function encode(str){
- 
+  return str.split('').map(x=>x.codePointAt(0).toString(16)).join(',');
 }
 
 function encodeArc(str) {
+  let encoded = encode(str).split(',');
+  console.log(encoded);
+
 
 }
 
